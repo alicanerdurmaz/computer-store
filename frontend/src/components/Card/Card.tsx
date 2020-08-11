456f357ad22d64ebfd61ea3c863d27f3f93da9dd
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-08-11 03:38:48 +0300

    button hover color changed

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -6,11 +6,26 @@ import styles from './Card.module.css'
 
 const Card = () => {
   return (
-    <div className={styles.item}>
+    <div
+      className={styles.item}
+      tabIndex={0}
+      onClick={() => alert('clicked product')}
+    >
       <div className={styles.container}>
-        <Image url={data.Images[1]} className={styles.img}></Image>
+        <Image url={data.Images[0]} className={styles.img}></Image>
         <h1 className={styles.name}> {data.Name}</h1>
-        <h1 className={styles.price}>${data.Price}</h1>
+        <div className={styles.footer}>
+          <h1 className={styles.price}>${data.Price}</h1>
+          <button
+            className={styles.button}
+            onClick={e => {
+              e.stopPropagation()
+              alert('clicked cart')
+            }}
+          >
+            ADD TO CART
+          </button>
+        </div>
       </div>
     </div>
   )
