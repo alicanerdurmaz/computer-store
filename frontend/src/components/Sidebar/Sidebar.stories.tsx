322f41e241061cac94a3d51e884ead128d982a0e
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-08-14 02:28:15 +0300

    storybook downgrade

--- frontend/src/components/Sidebar/Sidebar.stories.tsx
+++ frontend/src/components/Sidebar/Sidebar.stories.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 
 import Sidebar from './Sidebar'
+import filters from '../../test/data/filters.json'
 
 export default {
   component: Sidebar,
   title: 'Sidebar',
 }
 
-export const Default = async () => {
-  const res = await fetch(`http://localhost:3001/product/filters`)
-  const data = await res.json()
+export const Default = () => {
   return (
     <div style={{ width: '220px' }}>
-      <Sidebar filters={data} />
+      <Sidebar filters={filters} />
     </div>
   )
 }
