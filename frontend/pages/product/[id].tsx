7ae48d1cd2ccfe69029e0e71cc68424193f6ea94
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-08-22 11:27:02 +0300

    product detail

--- frontend/pages/product/[id].tsx
+++ frontend/pages/product/[id].tsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
-const Product = () => {
+interface Props {
+  product: any
+}
+
+const Product = ({ product }: Props) => {
+  console.log(product)
   return <div></div>
 }
 
 export default Product
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: true,
-  }
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch('http://localhost:3001/product/get-all-ids')
+  const ids = await res.json()
+
+  const paths = ids.map((id: string) => ({
+    params: { id: id },
+  }))
+
+  return { paths, fallback: false }
 }
 
-export async function getStaticProps() {}
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = await fetch(`http://localhost:3001/product/${params?.id}`)
+  const product = await res.json()
+
+  return { props: { product } }
+}
--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import cx from 'classnames'
 import styles from './logo.module.css'
-
+import Link from 'next/link'
 interface Props {
   className?: string
 }
 const Logo: React.FC<Props> = ({ className }: Props) => {
-  return <h1 className={cx(styles.logo, className)}>Computer&nbsp;Store</h1>
+  return (
+    <Link href="/">
+      <a className={cx(styles.logo, className)}>Computer&nbsp;Store</a>
+    </Link>
+  )
 }
 
 export default Logo
